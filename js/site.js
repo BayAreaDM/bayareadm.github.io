d28b6217c2962c04c5d92ea280ad479d0d589d02
Matt Gerstman <mattgerstman@gmail.com>
2015-02-14 09:46:44 -0800

    Navbar mostly works

--- js/site.js
+++ js/site.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
     animDuration: 250,              // Duration of jQuery animation
     startAt: 0,                     // Stick the menu at XXXpx from the top of the this() (nav container)
     easing: "linear",               // Easing type if jqueryEffects = true, use jQuery Easing plugin to extend easing types - gsgd.co.uk/sandbox/jquery/easing
-    animateCSS: true,               // AnimateCSS effect on/off
+    animateCSS: false,              // AnimateCSS effect on/off
     animateCSSRepeat: false,        // Repeat animation everytime user scrolls
     cssAnimation: "fadeInDown",     // AnimateCSS class that will be added to selector
     jqueryEffects: false,           // jQuery animation on/off
-    jqueryAnim: "slideDown",        // jQuery animation type: fadeIn, show or slideDown
+    jqueryAnim: "slideUp",          // jQuery animation type: fadeIn, show or slideDown
     selector: "a",                  // Selector to which activeClass will be added, either "a" or "li"
     mobile: false,                  // If false nav will not stick under 480px width of window
     mobileWidth: 480,               // The viewport width (without scrollbar) under which stickyNavbar will not be applied (due usability on mobile devices)
